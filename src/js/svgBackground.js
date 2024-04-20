fb6b63678f0d2dd3cfbd8b3c1136c7288357db95
1351618 <1351618@gmail.com>
2024-04-21 01:27:20 +0300

    add line

--- src/js/svgBackground.js
+++ src/js/svgBackground.js
@@ -1,17 +1,18 @@
 const svgUs = document.getElementById("svgBackground");
 
 const parentDiv = svgUs.parentElement; // Получаем родительский элемент
-
-let widthSvg = 0;
-let heightSvg = 0;
+/** width Svg */
+let ws = 0;
+/** height Svg */
+let hs = 0;
 
 const resizeObserver = new ResizeObserver((entries) => {
   for (let entry of entries) {
-    widthSvg = entry.contentRect.width;
-    heightSvg = entry.contentRect.height;
+    ws = entry.contentRect.width;
+    hs = entry.contentRect.height;
 
-    svgUs.setAttribute("width", widthSvg);
-    svgUs.setAttribute("height", heightSvg);
+    svgUs.setAttribute("width", ws);
+    svgUs.setAttribute("height", hs);
   }
 });
 
@@ -19,67 +20,65 @@ resizeObserver.observe(parentDiv);
 
 const figureColor = "#34c300";
 
-function circlePath(percentage, R, W, rotation) {
-  const circumference = W * Math.PI;
+let degrees = 0;
+setInterval(() => {
+  degrees === 360 ? (degrees = 0) : (degrees += 0.1);
+  renering();
+}, 10);
+
+/**
+ * функция отрисовки кругов
+ * @param {*} SD количество линий
+ * @param {*} R радиус
+ * @param {*} D градусы поворота
+ */
+function circle(SD, R, D) {
+  // Создание круга
   const circle = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "circle"
   );
-  circle.setAttribute("cx", widthSvg / 2);
-  circle.setAttribute("cy", heightSvg / 2);
+  circle.setAttribute("cx", ws / 2);
+  circle.setAttribute("cy", hs / 2);
   circle.setAttribute("r", R);
   circle.setAttribute("fill", "none");
   circle.setAttribute("stroke", figureColor);
   circle.style.filter = `drop-shadow(0 0 1px ${figureColor}) drop-shadow(0 0 2px ${figureColor}) drop-shadow(0 0 3px ${figureColor})`;
   circle.setAttribute("stroke-width", "2");
-  circle.setAttribute("stroke-dasharray", `${circumference}`);
-
-  circle.setAttribute(
-    "stroke-dashoffset",
-    (1 - percentage / 100) * circumference
-  );
-  circle.setAttribute(
-    "transform",
-    `rotate(${rotation}, ${widthSvg / 2}, ${heightSvg / 2})`
-  );
-
+  circle.setAttribute("stroke-dasharray", `${SD}`);
+  circle.setAttribute("transform", `rotate(${D}, ${ws / 2}, ${hs / 2})`);
   svgUs.appendChild(circle);
 }
 
-let percent = 0;
-setInterval(() => {
-  percent === 360 ? (percent = 0) : (percent += 0.1);
-  renering();
-}, 10);
+function line(SD, X2, Y2, D) {
+  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  line.setAttribute("x1", ws / 2);
+  line.setAttribute("y1", hs / 2);
+  line.setAttribute("x2", X2);
+  line.setAttribute("y2", Y2);
+  line.setAttribute("stroke", figureColor);
+  line.style.filter = `drop-shadow(0 0 1px ${figureColor}) drop-shadow(0 0 2px ${figureColor}) drop-shadow(0 0 3px ${figureColor})`;
+  line.setAttribute("stroke-width", "2");
+  line.setAttribute("stroke-dasharray", `${SD}`);
+  line.setAttribute("transform", `rotate(${D}, ${ws / 2}, ${hs / 2})`);
+  svgUs.appendChild(line);
+}
 
 function renering() {
   svgUs.innerHTML = "";
-  circlePath(80, 20, 8, percent);
-  circlePath(50, 80, 10, percent);
-  circlePath(20, 300, 80, percent);
+
+  circle(400, 400, 360 - degrees * 2);
+  circle(15, 20, degrees * 2);
+  circle(200, 200, 360 - degrees);
+  circle(110, 150, degrees - 6);
+  circle(400, 300, 0);
+  circle(50, 56, 0);
+  circle(0, 500, 0);
+  circle(0, 320, 0);
+  circle(0, 78, 0);
+
+  line(150, 200, 200, degrees);
+  line(100, 200, 200, 360 - degrees);
+  line(0, 200, 200, 155);
+  line(150, ws / 2, hs / 3.8, degrees + 60);
 }
-
-// // Создание круга
-// const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-// circle.setAttribute("cx", 300);
-// circle.setAttribute("cy", 300);
-// circle.setAttribute("r", 50);
-// circle.setAttribute("fill", "none");
-// circle.setAttribute("stroke", "#000");
-// circle.setAttribute("stroke-width", "1");
-// svgUs.appendChild(circle);
-
-// // Создание дуги
-// const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-// path.setAttribute(
-//   "d",
-//   `M ${400 + 20 * Math.cos((0 * Math.PI) / 180)} ${
-//     400 + 20 * Math.sin((0 * Math.PI) / 180)
-//   } A ${20} ${20} 0 ${345 - 0 <= 180 ? 0 : 1} 1 ${
-//     400 + 20 * Math.cos((345 * Math.PI) / 180)
-//   } ${400 + 20 * Math.sin((345 * Math.PI) / 180)}`
-// );
-// path.setAttribute("fill", "none");
-// path.setAttribute("stroke", "#000");
-// path.setAttribute("stroke-width", "1");
-// svgUs.appendChild(path);
