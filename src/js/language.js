b41b6a0c1eaa65bf9c9371e35eb240e039785544
1351618 <1351618@gmail.com>
2024-09-12 20:11:36 +0300

    line rew

--- src/js/language.js
+++ src/js/language.js
@@ -36,24 +36,42 @@ function translation() {
   if (data) {
     const selectedLang = lang[langIndex]; // Выбранный язык по текущему индексу
     localStorage.setItem("selectedLang", selectedLang);
-    const writeLeng = data.find((item) => item.hasOwnProperty(selectedLang))[
-      selectedLang
-    ];
+    const writeLeng = data[selectedLang];
+
     DG("name").innerHTML = writeLeng.main.name;
     DG("jobTitle").innerHTML = writeLeng.main.jobTitle;
     DG("intro").innerHTML = writeLeng.aboutMe.aboutMeText;
     DG("skills").querySelector("h2").innerHTML = writeLeng.techStack.title;
+    DG("experience").querySelector("h2").innerHTML =
+      writeLeng.devAchievements.title;
+    DG("experienceCont").innerHTML = writeLeng.devAchievements.workplaces
+      .map((val) => {
+        return `
+            <div class="experience-item">
+              <h3>${val.jobTitle}</h3>
+              <p class="date">${val.company} / ${val.period}</p>
+             
+              <ul>
+                ${val.achievements.map((item) => `<li>${item}</li>`).join("")}
+              </ul>
+            </div>`;
+      })
+      .join("");
     DG("works").querySelector("h2").innerHTML = writeLeng.projects.title;
-    //   document.getElementById("education").innerHTML =
-    //     writeLeng.section1.education;
-    //   document.getElementById("educationText").innerHTML =
-    //     writeLeng.section1.educationText;
-    //   document.getElementById("workExperience").innerHTML =
-    //     writeLeng.section2.workExperience;
-    //   document.getElementById("workExperienceText").innerHTML =
-    //     writeLeng.section2.workExperienceText;
-    //   document.getElementById("skills").innerHTML = writeLeng.section2.skills;
-    //   document.getElementById("skillsText").innerHTML =
-    //     writeLeng.section2.skillsText;
+    DG("contact").querySelector("h2").innerHTML = writeLeng.contacts.title;
+    DG("projects").querySelector("h2").innerHTML = writeLeng.section3.title;
+
+    DG("projects-grid").innerHTML = writeLeng.section3.section3Cont
+      .map((val) => {
+        return `
+      <div class="project-item">
+        <h3 class="project-title">${val.name}</h3>
+        <p class="project-description">
+          ${val.description}
+        </p>
+      </div>
+      `;
+      })
+      .join("");
   }
 }
